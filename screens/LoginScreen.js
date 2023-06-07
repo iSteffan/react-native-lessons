2b0472513de019868e1ca6b0e6897d0fcaac90fa
Stepan Skichko <st.skichko@gmail.com>
2023-06-07 12:58:14 +0300

    loginscreen DONE

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,111 +1,46 @@
 import { StyleSheet } from 'react-native';
-import {
-  ImageBackground,
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  KeyboardAvoidingView,
-  TouchableWithoutFeedback,
-} from 'react-native';
+import { ImageBackground, View, Text, TextInput, TouchableOpacity } from 'react-native';
 
-import { useState } from 'react';
-
-export const LoginScreen = ({ navigation }) => {
-  const [focused, setFocused] = useState('');
-  const [showPassword, setShowPassword] = useState(false);
-  const [formData, setFormData] = useState({
-    email: '',
-    password: '',
-  });
-
-  const { email, password } = formData;
-
-  function onChange(e) {
-    setFormData(prevState => ({
-      ...prevState,
-      [e.target.id]: e.target.value,
-    }));
-  }
-
-  function togglePassword() {
-    setShowPassword(prevState => !prevState);
-  }
+export const LoginScreen = () => {
   return (
-    <TouchableWithoutFeedback>
-      <View style={styles.mainContainer}>
-        <ImageBackground
-          source={require('../images/photo-bg.jpg')}
-          resizeMode="cover"
-          style={styles.image}
-        >
-          <KeyboardAvoidingView style={styles.keyboard}>
-            <View style={styles.container}>
-              <Text style={styles.title}>Увійти</Text>
-              <View style={styles.form}>
-                <TextInput
-                  id="email"
-                  value={email}
-                  onChange={onChange}
-                  placeholder="Адреса електронної пошти"
-                  autoCompleteType="email"
-                  autoCapitalize="none"
-                  selectionColor={'#FF6C00'}
-                  onFocus={() => {
-                    setFocused('email');
-                  }}
-                  onBlur={() => {
-                    setFocused('');
-                  }}
-                  style={{
-                    ...styles.input,
-                    borderColor: focused === 'email' ? '#FF6C00' : '#E8E8E8',
-                  }}
-                />
-                <View style={styles.passwordContainer}>
-                  <TextInput
-                    type={showPassword ? 'text' : 'password'}
-                    id="password"
-                    value={password}
-                    onChange={onChange}
-                    placeholder="Пароль"
-                    secureTextEntry={showPassword}
-                    autoCompleteType="password"
-                    autoCapitalize="none"
-                    selectionColor={'#FF6C00'}
-                    onFocus={() => {
-                      setFocused('password');
-                    }}
-                    onBlur={() => {
-                      setFocused('');
-                    }}
-                    style={{
-                      ...styles.input,
-                      borderColor: focused === 'password' ? '#FF6C00' : '#E8E8E8',
-                    }}
-                  />
-                  <TouchableOpacity onPress={togglePassword} style={styles.passwordIndicator}>
-                    <Text style={styles.showText}>{showPassword ? 'Показати' : 'Сховати'}</Text>
-                  </TouchableOpacity>
-                </View>
-                <TouchableOpacity style={styles.btn}>
-                  <Text style={styles.BtnLogIn}>Увійти</Text>
-                </TouchableOpacity>
-                <View style={styles.wrapper}>
-                  <Text style={styles.loginText}>Немає акаунту?</Text>
-                  <TouchableOpacity
-                    onPress={() => navigation.navigate('RegistrationScreen')}
-                    style={styles.loginText}
-                  >
-                    <Text style={styles.loginLink}>Зареєструватися</Text>
-                  </TouchableOpacity>
-                </View>
-              </View>
+    <View style={styles.mainContainer}>
+      <ImageBackground
+        source={require('../images/photo-bg.jpg')}
+        resizeMode="cover"
+        style={styles.image}
+      >
+        <View style={styles.container}>
+          <Text style={styles.title}>Увійти</Text>
+          <View style={styles.form}>
+            <TextInput
+              placeholder="Адреса електронної пошти"
+              autoCompleteType="email"
+              autoCapitalize="none"
+              selectionColor={'#FF6C00'}
+              style={styles.input}
+            />
+            <View style={styles.passwordContainer}>
+              <TextInput
+                placeholder="Пароль"
+                autoCompleteType="password"
+                autoCapitalize="none"
+                selectionColor={'#FF6C00'}
+                style={styles.input}
+              />
             </View>
-          </KeyboardAvoidingView>
-        </ImageBackground>
-      </View>
-    </TouchableWithoutFeedback>
+            <TouchableOpacity style={styles.btn}>
+              <Text style={styles.BtnLogIn}>Увійти</Text>
+            </TouchableOpacity>
+            <View style={styles.wrapper}>
+              <Text style={styles.loginText}>Немає акаунту?</Text>
+              <TouchableOpacity style={styles.loginText}>
+                <Text style={styles.loginLink}>Зареєструватися</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </ImageBackground>
+    </View>
   );
 };
 
@@ -120,15 +55,6 @@ const styles = StyleSheet.create({
   image: {
     flex: 1,
     justifyContent: 'flex-end',
-  },
-
-  keyboard: {
-    flex: 1,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-
-    height: '100%',
-    width: '100%',
   },
 
   container: {
@@ -183,20 +109,6 @@ const styles = StyleSheet.create({
     marginBottom: 43,
   },
 
-  passwordIndicator: {
-    position: 'absolute',
-    top: '50%',
-    right: 16,
-    transform: [{ translateY: -6 }],
-  },
-
-  showText: {
-    fontFamily: 'Roboto_400Regular',
-    fontSize: 16,
-    lineHeight: 19,
-    color: '#1B4371',
-  },
-
   btn: {
     display: 'flex',
     alignItems: 'center',
@@ -217,14 +129,6 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#fff',
   },
-
-  //   loginBtn: {
-  //     marginLeft: 'auto',
-  //     marginRight: 'auto',
-
-  //     width: 188,
-  //     height: 19,
-  //   },
 
   wrapper: {
     display: 'flex',
